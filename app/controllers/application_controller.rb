801e93337399236adf125397b125308ef289616e
Frakhanda Shah <fbshah@hotmail.co.uk>
2016-01-10 03:06:55 +0000

    Added fields tO Profile.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,7 @@ class ApplicationController < ActionController::Base
   
   private
 
+
   def mailbox
     @mailbox ||= current_user.mailbox
   end
@@ -19,9 +20,15 @@ class ApplicationController < ActionController::Base
 
 
   protected
+  
+  def user_params
+  params.require(:user).permit(:employee_no, :first_name, :last_name, :date_of_birth, :email, :address, :mobile, :contract_type, :role, :ni, :work_permit, :password) 
+end
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :name
-    devise_parameter_sanitizer.for(:account_update) << :name
+
+ def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up)  { |u| u.permit(:first_name, :last_name, :email, :password) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:employee_no, :first_name, :last_name, :date_of_birth, :email, :address, :mobile, :contract_type, :role, :ni, :work_permit, :password) }
   end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
- # encoding: UTF-8
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,6 +12,40 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20160109003933) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.integer  "failed_attempts",        default: 0,  null: false
+    t.string   "unlock_token"
+    t.datetime "locked_at"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "admins", ["confirmation_token"], name: "index_admins_on_confirmation_token", unique: true
+  add_index "admins", ["email"], name: "index_admins_on_email", unique: true
+  add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  add_index "admins", ["unlock_token"], name: "index_admins_on_unlock_token", unique: true
+
+  create_table "contacts", force: :cascade do |t|
+    t.string   "mobile",     null: false
+    t.string   "address",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "mailboxer_conversation_opt_outs", force: :cascade do |t|
     t.integer "unsubscriber_id"
@@ -89,6 +123,14 @@ ActiveRecord::Schema.define(version: 20160109003933) do
     t.string   "first_name"
     t.string   "last_name"
     t.datetime "date_of_birth"
+    t.string   "mobile"
+    t.string   "address"
+    t.string   "role"
+    t.string   "contract_type"
+    t.integer  "assigned_hours"
+    t.string   "NI"
+    t.string   "work_permit"
+    t.string   "employee_no"
     t.boolean  "admin"
   end
 
