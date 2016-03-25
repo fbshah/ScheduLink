6c84cdd33dbc1f51c8e4352f9d76265a7b9c9e3b
Frakhanda Shah <fbshah@hotmail.co.uk>
2016-03-25 19:59:11 +0000

    resource added

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,8 @@ Rails.application.routes.draw do
  
 
 
-  devise_for :users, :controllers => { registrations: 'registrations' }
-  resources :users
+  devise_for :users
+      resources :users
  
 
   
