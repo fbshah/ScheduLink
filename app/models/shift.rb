cf49ae8740727cf36a7933be598d319c379e6688
Mohammed Rashid <rashid_92@hotmail.co.uk>
2016-04-07 22:42:54 +0100

    create a shift for a user and validate

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,3 +1,11 @@
 class Shift < ActiveRecord::Base
 	belongs_to :user
+	validates :date, uniqueness: { scope: :user, message: "has already been taken for this user" }
+	validate :min_two_hour
+
+	def min_two_hour
+		if Time.diff(start_time, end_time)[:hour] < 2
+			errors.add(:end_time, "minimum two hour shift")
+		end
+	end
 end
