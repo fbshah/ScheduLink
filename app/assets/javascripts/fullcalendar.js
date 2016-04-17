d528256d34246744f79b14ef740f270106d5d031
Frakhanda Shah <fbshah@hotmail.co.uk>
2016-04-17 19:42:29 +0100

    fullcalendar and events

--- app/assets/javascripts/fullcalendar.js
+++ app/assets/javascripts/fullcalendar.js
@@ -1,10 +1,18 @@
         $(function() { // document ready
-
-
+var myCalendar = $('#my-calendar-id'); 
+myCalendar.fullCalendar();
+var myEvent = {
+  title:"my new event",
+  allDay: true,
+  start: new Date(),
+  end: new Date()
+};
+myCalendar.fullCalendar( 'renderEvent', myEvent );
             $('#calendar').fullCalendar({
                 
 
                 events: '/events.json',
+
 
                 defaultView: 'agendaDay',
                 editable: true,
@@ -19,17 +27,17 @@
                     agendaSevenDay: {
                         type: 'agenda',
                         duration: { days: 7 },
-                        groupByResource: true
+                        groupByResource: false
                     }
                 },
 
                 //// uncomment this line to hide the all-day slot
                 allDaySlot: false,
 
-                 events: [
+                events: [
                     { id: '1', resourceId: 'a', start: '2016-04-17', end: '2016-04-19', title: 'event 1' },
                     { id: '2', resourceId: 'a', start: '2016-04-18T09:00:00', end: '2016-04-19T14:00:00', title: 'event 2' },
-                    { id: '3', resourceId: 'b', start: '2016-04-18T12:00:00', end: '2015-12-08T06:00:00', title: 'event 3' },
+                    { id: '3', resourceId: 'b', start: '2016-04-17T12:00:00', end: '2016-04-17T06:00:00', title: 'event 3' },
                     { id: '4', resourceId: 'c', start: '2015-12-07T07:30:00', end: '2015-12-07T09:30:00', title: 'event 4' },
                     { id: '5', resourceId: 'd', start: '2015-12-07T10:00:00', end: '2015-12-07T15:00:00', title: 'event 5' }
                 ],
