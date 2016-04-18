0706817dbdacc39bd62efd79a3cf856813488d32
Mohammed Rashid <rashid_92@hotmail.co.uk>
2016-04-18 19:10:49 +0100

    change url for production mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { :host => 'intense-sierra-39639.herokuapp.com', :protocol => 'http' }
+  config.action_mailer.default_url_options = { :host => 'tranquil-thicket-51623.herokuapp.com', :protocol => 'http' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
